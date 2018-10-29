ce83c99ff4c8c50c5bb08ff1cf5fa43367616e89
Andre <andre.jacquemond@heig-vd.ch>
2018-10-29 22:59:10 +0100

    Ajout pagination page dev

--- src/main/java/ch/heigvd/amt/mvcprojet/presentation/DeveloperServlet.java
+++ src/main/java/ch/heigvd/amt/mvcprojet/presentation/DeveloperServlet.java
@@ -1,6 +1,7 @@
 package ch.heigvd.amt.mvcprojet.presentation;
 
 import ch.heigvd.amt.mvcprojet.database.ApplicationDAO;
+import ch.heigvd.amt.mvcprojet.model.Application;
 import ch.heigvd.amt.mvcprojet.model.User;
 
 import javax.ejb.EJB;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,9 +26,30 @@ public class DeveloperServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
+
+        List<Application> list = applicationDAO.findUserApplication(user.getUserId());
+        // Permet la pagination
+        int page = 1;
+        int recordPerPage = 5;
+        if(request.getParameter("page") != null)
+            page = Integer.parseInt(request.getParameter("page"));
+        int nbApp = list.size();
+        int nbPage = (int) Math.ceil(nbApp * 1.0 / recordPerPage);
+        if (page == 1){
+            list = list.subList((page-1), (page*recordPerPage));
+        }
+        else if(page*recordPerPage >= nbApp){
+            list = list.subList(((page -1) *recordPerPage), nbApp);
+        }
+        else{
+            list = list.subList((page*recordPerPage), (page*recordPerPage + recordPerPage));
+        }
+        request.setAttribute("nbPage", nbPage);
+        request.setAttribute("page", page);
+
         response.setContentType("text/html;charset=UTF-8");
         LOGGER.log(Level.INFO, "list", session.getAttributeNames());
-        request.setAttribute("applications", applicationDAO.findUserApplication(user.getUserId()));
+        request.setAttribute("applications", list);
         request.getRequestDispatcher("/WEB-INF/pages/dev.jsp").forward(request, response);
     }
 }
