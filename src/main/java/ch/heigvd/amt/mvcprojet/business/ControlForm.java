78ed3e82d7dba99a992c6847444d751afcb46db3
Alexandre Vouilloz <alex_vouilloz@netplus.ch>
2018-11-12 19:10:17 +0100

    Delete unused files

--- src/main/java/ch/heigvd/amt/mvcprojet/business/ControlForm.java
+++
@@ -1,8 +0,0 @@
-package ch.heigvd.amt.mvcprojet.business;
-
-public class ControlForm {
-
-    public static boolean ControlForm(String firstName, String lastName, String email, String password, String password2){
-        return false;
-    }
-}
