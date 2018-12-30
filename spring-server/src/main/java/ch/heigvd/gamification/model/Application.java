7578f82be747fcb96d7ff8e7140f342102d97931
Andre <andre.jacquemond@heig-vd.ch>
2018-12-30 21:49:41 +0100

    commit Application pour tenter de coller au data.sql

--- spring-server/src/main/java/ch/heigvd/gamification/model/Application.java
+++ spring-server/src/main/java/ch/heigvd/gamification/model/Application.java
@@ -1,34 +1,45 @@
 package ch.heigvd.gamification.model;
 
 import java.io.Serializable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  *
  * @author Olivier Liechti
  */
 @Entity
+@Table(name = "Application")
 public class Application implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    @Column(nullable = false)
+    private long appId;
 
-    @Column(unique = true)
+    @Column(nullable = false)
     private String name;
 
-    private String passwordHash;
+    @Column(nullable = false)
+    private String keyUUID;
 
-    public long getId() {
-        return id;
+    @Column(nullable = false)
+    private String secretUUID;
+
+    public Application(){}
+
+    public Application(int id, String name, String keyUUID, String secretUUID){
+        this.appId = id;
+        this.name = name;
+        this.keyUUID = keyUUID;
+        this.secretUUID = secretUUID;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public long getAppId() {
+        return appId;
+    }
+
+    public void setAppId(long appId) {
+        this.appId = appId;
     }
 
     public String getName() {
@@ -39,12 +50,20 @@ public class Application implements Serializable {
         this.name = name;
     }
 
-    public String getPasswordHash() {
-        return passwordHash;
+    public String getKeyUUID() {
+        return keyUUID;
     }
 
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
+    public void setKeyUUID(String keyUUID) {
+        this.keyUUID = keyUUID;
+    }
+
+    public String getSecretUUID() {
+        return secretUUID;
+    }
+
+    public void setSecretUUID(String keyUUID) {
+        this.secretUUID = keyUUID;
     }
 
 }
