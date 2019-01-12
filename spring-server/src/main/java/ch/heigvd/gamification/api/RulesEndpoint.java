adad29926a8ec0f94521f61be3caa5449711fee2
Alexandre Vouilloz <alex_vouilloz@netplus.ch>
2019-01-12 16:05:34 +0100

    Fixed rule endpoint

--- spring-server/src/main/java/ch/heigvd/gamification/api/RulesEndpoint.java
+++ spring-server/src/main/java/ch/heigvd/gamification/api/RulesEndpoint.java
@@ -3,19 +3,16 @@ package ch.heigvd.gamification.api;
 import ch.heigvd.gamification.api.dto.*;
 import ch.heigvd.gamification.dao.ApplicationRepository;
 import ch.heigvd.gamification.dao.EndUserRepository;
-import ch.heigvd.gamification.dao.RuleRepository;
+import ch.heigvd.gamification.dao.PointRuleRepository;
 import ch.heigvd.gamification.model.Application;
-import ch.heigvd.gamification.model.Rule;
+import ch.heigvd.gamification.model.PointRule;
+import ch.heigvd.gamification.model.PointRuleParam;
 import ch.heigvd.gamification.services.EventProcessor;
-import io.swagger.annotations.ApiParam;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,60 +20,39 @@ import java.util.List;
 public class RulesEndpoint implements RulesApi {
 
   private final ApplicationRepository applicationsRepository;
-  private final RuleRepository ruleRepository;
+  private final PointRuleRepository pointRuleRepository;
   private final EventProcessor eventProcessor;
 
-  public RulesEndpoint(ApplicationRepository applicationsRepository, RuleRepository ruleRepository, EndUserRepository endUsersRepository, EventProcessor eventProcessor) {
+  public RulesEndpoint(ApplicationRepository applicationsRepository, PointRuleRepository pointRuleRepository, EndUserRepository endUsersRepository, EventProcessor eventProcessor) {
     this.applicationsRepository = applicationsRepository;
-    this.ruleRepository = ruleRepository;
+    this.pointRuleRepository = pointRuleRepository;
     this.eventProcessor = eventProcessor;
   }
 
   @Override
-  public ResponseEntity<List<ApplicationsRulesSummary>> findApplicationRules(@ApiParam(value = "uuid de l'application à trouver",required=true ) @PathVariable("uuid") String uuid){
-    List<ApplicationsRulesSummary> result = new ArrayList<>();
-    // TODO: test pour savoir si le uuid est correct?
-    Application app = applicationsRepository.findByKeyUUID(uuid);
-    // TODO: test à implémenter pour savoir si on a une application (sinon renvoi code http correspondant)
-    if(app == null){
-      return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-    }
-    // TODO: test à implémenter pour savoir si on a une règle ?
-    for (Rule rule : app.getRules()){
-      ApplicationsRulesSummary rs = new ApplicationsRulesSummary();
-      rs.setRulesName(rule.getName());
-      rs.setRuleType(rule.getType());
-      rs.setNumberOfPoint(Integer.toString(rule.getNumberOfPoint()));
-      result.add(rs);
-    }
-    return ResponseEntity.ok(result);
-  }
-
-  @Override
-  public ResponseEntity<Void> postRule(@ApiParam(value = "uuid of the application to add rule",required=true ) @PathVariable("uuid") String uuid,
-                                       @ApiParam(value = "The rule for an application's badges" ,required=true ) @RequestBody RegistrationRule body) {
-    Rule newRule = new Rule();
+  public ResponseEntity<Void> createPointRule(String uuid, PointRuleDto body) {
     // TODO: test à implémenter pour savoir si on a un nom (sinon renvoi code http correspondant) => André OK
-    if(body.getRuleName().isEmpty() || body.getRuleType().isEmpty() || uuid.isEmpty()){
+    if(body.getName().isEmpty() || uuid.isEmpty()){
       return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
     }
-    newRule.setName(body.getRuleName());
-    newRule.setType(body.getRuleType());
-    newRule.setNumberOfPoint(body.getNumberOfPoint());
-    Application applicationNewRule = applicationsRepository.findByKeyUUID(uuid);
+
+    Application app = applicationsRepository.findByKeyUUID(uuid);
     try {
       // TODO: test à implémenter pour savoir si on a une application (sinon renvoi code http correspondant) => André OK
       // TODO: test à implémenter pour savoir si le secret match celui de l'application (sinon renvoi code http correspondant) => André OK
-      if(applicationNewRule == null){
+      if(app == null){
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
       }
-      if(!applicationNewRule.getSecretUUID().equalsIgnoreCase(body.getApplicationSecret())){
+      if(!app.getSecretUUID().equalsIgnoreCase(body.getApplicationSecret())){
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
       }
-      applicationNewRule.getRules().add(newRule);
-      ruleRepository.save(newRule);
-      //applicationsRepository.save(applicationNewRule);
+
+      PointRule newRule = dtoToModel(body);
+      app.getPointRules().add(newRule);
+      pointRuleRepository.save(newRule);
+      applicationsRepository.save(app);
       return ResponseEntity.status(HttpStatus.CREATED).build();
+
     } catch (DataIntegrityViolationException e) {
       System.out.println(e.getMessage());
       System.out.println(e.getClass());
@@ -85,14 +61,116 @@ public class RulesEndpoint implements RulesApi {
   }
 
   @Override
-  public ResponseEntity<Void> updateRule(@ApiParam(value = "uuid of the application to update rule",required=true ) @PathVariable("uuid") String uuid,
-                                         @ApiParam(value = "The rule for an application's badges" ,required=true ) @RequestBody UpdateRule body) {
+  public ResponseEntity<List<PointRuleDto>> findPointRules(String uuid) {
+    List<PointRuleDto> result = new ArrayList<>();
+    // TODO: test pour savoir si le uuid est correct?
     Application app = applicationsRepository.findByKeyUUID(uuid);
-    Rule updateRule = ruleRepository.findByNameAndApplication(body.getOldRuleName(), app);
-    updateRule.setName(body.getRuleName());
-    updateRule.setType(body.getRuleType());
-    updateRule.setNumberOfPoint(body.getNumberOfPoint());
-    ruleRepository.save(updateRule);
+    // TODO: test à implémenter pour savoir si on a une application (sinon renvoi code http correspondant)
+    if(app == null){
+      return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
+    // TODO: test à implémenter pour savoir si on a une règle ?
+    for (PointRule rule : app.getPointRules()) {
+      result.add(modelToDto(rule));
+    }
+    return ResponseEntity.ok(result);
+  }
+
+  @Override
+  public ResponseEntity<Void> updatePointRule(String uuid, PointRuleUpdateDto body) {
+    Application app = applicationsRepository.findByKeyUUID(uuid);
+    PointRule updatedRule = pointRuleRepository.findByNameAndApp(body.getOldName(), app);
+    updatedRule.setName(body.getNewName());
+    updatedRule.setEventType(body.getNewEventType());
+    updatedRule.setScale(body.getNewScale());
+    updatedRule.setDefaultNbPoints(body.getNewDefaultNbPoints());
+    List<PointRuleParam> params = new ArrayList<>();
+    for(PointRuleParamDto paramDto: body.getNewPointRuleParams()) {
+      params.add(dtoToModel(paramDto));
+    }
+    updatedRule.setPointRuleParams(params);
+    pointRuleRepository.save(updatedRule);
     return ResponseEntity.status(HttpStatus.OK).build();
+  }
+
+  @Override
+  public ResponseEntity<Void> createBadgeThresholdRule(String uuid, BadgeThresholdRuleDto body) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<List<BadgeThresholdRuleDto>> findBadgeThresholdRules(String uuid) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Void> updateBadgeThresholdRule(String uuid, BadgeThresholdRuleUpdateDto body) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Void> createBadgeTimeRangeRule(String uuid, BadgeTimeRangeRuleDto body) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<List<BadgeTimeRangeRuleDto>> findBadgeTimeRangeRules(String uuid) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Void> updateBadgeTimeRangeRule(String uuid, BadgeTimeRangeRuleUpdateDto body) {
+    return null;
+  }
+
+  /** Converts a <code>PointRule</code> to a <code>PointRuleDto</code>*/
+  public static PointRuleDto modelToDto(PointRule rule) {
+    PointRuleDto ruleDto = new PointRuleDto();
+    ruleDto.setName(rule.getName());
+    ruleDto.setEventType(rule.getEventType());
+    ruleDto.setScale(rule.getScale());
+    ruleDto.setDefaultNbPoints(rule.getDefaultNbPoints());
+
+    List<PointRuleParamDto> ruleParamsDto= new ArrayList<>();
+    for(PointRuleParam param : rule.getPointRuleParams()) {
+      PointRuleParamDto paramDto = modelToDto(param);
+      ruleParamsDto.add(paramDto);
+    }
+    return ruleDto;
+  }
+
+  /** Converts a <code>PointRuleParam</code> to a <code>PointRuleParamDto</code>*/
+  public static PointRuleParamDto modelToDto(PointRuleParam param) {
+    PointRuleParamDto paramDto = new PointRuleParamDto();
+    paramDto.setParamName(param.getParamName());
+    paramDto.setParamValue(param.getParamValue());
+    paramDto.setNbPoints(param.getNbPoints());
+    return paramDto;
+  }
+
+  /** Converts a <code>PointRuleDto</code> to a <code>PointRule</code>*/
+  public static PointRule dtoToModel(PointRuleDto ruleDto) {
+    PointRule rule = new PointRule();
+    rule.setName(ruleDto.getName());
+    rule.setEventType(ruleDto.getEventType());
+    rule.setScale(ruleDto.getScale());
+    rule.setDefaultNbPoints(ruleDto.getDefaultNbPoints());
+
+    List<PointRuleParam> ruleParams= new ArrayList<>();
+    for(PointRuleParamDto paramDto : ruleDto.getPointRuleParams()) {
+      PointRuleParam param = dtoToModel(paramDto);
+      param.setPointRule(rule);
+      ruleParams.add(param);
+    }
+    return rule;
+  }
+
+  /** Converts a <code>PointRuleParamDto</code> to a <code>PointRuleParam</code>*/
+  public static PointRuleParam dtoToModel(PointRuleParamDto paramDto) {
+    PointRuleParam param = new PointRuleParam();
+    param.setParamName(paramDto.getParamName());
+    param.setParamValue(paramDto.getParamValue());
+    param.setNbPoints(paramDto.getNbPoints());
+    return param;
   }
 }
