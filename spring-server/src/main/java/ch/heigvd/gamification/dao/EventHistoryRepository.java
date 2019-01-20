be933c80b58635a8d5177b51e9cf80fbc70680c3
silverkameni <silverkameni@yahoo.fr>
2019-01-20 17:11:52 +0100

    dokerrize OK

--- spring-server/src/main/java/ch/heigvd/gamification/dao/EventHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.heigvd.gamification.dao;
-
-import ch.heigvd.gamification.model.EndUser;
-import ch.heigvd.gamification.model.EventHistory;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EventHistoryRepository  extends CrudRepository<EventHistory, Long> {
-    EventHistory findByUserAndEventType(EndUser user, String eventType);
-}
